9ea8f625daccc7b809eadff9a4f26b09476aeb7e
RA Studio <thelightvizlink@gmail.com>
2026-01-03 17:06:34 +0100

    Fix: Remove basename from BrowserRouter to fix black screen on Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter basename={import.meta.env.BASE_URL}>
+          <BrowserRouter>
             <CustomCursor />
             <Navigation />
             <Routes>
